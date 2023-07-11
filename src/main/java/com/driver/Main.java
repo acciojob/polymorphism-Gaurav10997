a41b8d545634a9dd3eabe0ff71f91cb3889a28e3
gaurav mandal <avdhika@gmail.com>
2023-07-11 16:12:12 +0530

    origin

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,13 +4,14 @@ public class Main {
     public class Product{
      public int product(int x, int y) {
 
+        return x+y;
      }
      public int product(int x, int y, int z) {
-
+ return x+y+z;
      }
 
      public double product(double x, double y) {
-
+ return x+y;
      }
 
 
