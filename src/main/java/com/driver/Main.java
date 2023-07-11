22cc95b04d85aee470e25fa943da37a549691e0a
gaurav mandal <avdhika@gmail.com>
2023-07-11 16:03:36 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
+    public class Product {
+        public int product(int x, int y) {}
+    }
 
+
+
+    public static void main(String[] args) {
+        
+    }
 }
